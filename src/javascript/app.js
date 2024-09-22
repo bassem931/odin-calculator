b3475489cf8444a1a9963e220a7bbdaa307c2c10
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-09-23 00:39:32 +0300

    feat: add logic to enter number
    add numbers and operators and save into variables and add to stack array
    next step is to calculate the results then work on displaying the values

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,3 +1,13 @@
+const calculatorStack = [];
+let operand1="";
+let operator="";
+let operand2="";
+const numbers = "0123456789"
+const operators = "+-/*="
+let isOperand1Mode = true;
+let isOperand2Mode = false;
+let isOperatorMode = false;
+
 function add(a,b) {
     return a+b;
 }
@@ -11,5 +21,47 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if(b===0){
+        return "cannot divide by zero"
+}
     return a/b;
 }
+
+function updateValues (buttonVal){
+    if(numbers.includes(buttonVal)){
+        console.log(isOperand1Mode,buttonVal);
+        if(isOperand1Mode){
+            operand1 = operand1 + buttonVal;
+        } else if(isOperand2Mode){
+            operand2 = operand2 + buttonVal
+        }
+    } else if(operators.includes(buttonVal)){
+        if(isOperand1Mode){
+            calculatorStack.push(operand1,buttonVal);
+            isOperand1Mode = false;
+            isOperand2Mode = true;
+        }
+        if(isOperand2Mode){
+
+        }
+        
+    }
+    console.log(operand1,calculatorStack,operand2);
+    
+}
+
+
+function addButtonListeners(){
+    const buttons = document.querySelectorAll(".calc-button");
+    for (const button of buttons) {
+        button.addEventListener("click",(ev)=>{
+            let buttonVal = button.textContent.trim();
+            if(buttonVal==="C"){
+                //clear function
+            }
+            updateValues(buttonVal);
+        })
+    }
+}
+
+addButtonListeners()
